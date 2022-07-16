5fefb2fa201f2d1e4771e849f532bd86c44e3065
denis-spe <denisbrian07@gmail.com>
2022-07-16 17:51:52 +0200

    using the window innerheight to fit the intro page

--- Hello/static/js/index.js
+++ Hello/static/js/index.js
@@ -1,1 +1,5 @@
-alert(window.innerHeight)
+// Get the class.
+const  flaskLogoContainer = document.querySelector('.flask-logo-container');
+
+// set the height of the class.
+flaskLogoContainer.style.height = window.innerHeight
